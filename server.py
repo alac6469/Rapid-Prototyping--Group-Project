1fb7146c6575bc1906a790ddf5d08b13ffa5e0b7
Sam Beckett <mojobeckett@Sam-Becketts-MacBook-Pro.local>
2014-04-14 20:19:16 -0600

    Adding working sign in, create.html and profile.html

--- server.py
+++ server.py
@@ -16,18 +16,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bids.db'
 db = SQLAlchemy(app)
 user = None
 app.debug = True
-'''
+
 class Users(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	user_name = db.Column(db.String)
-	password = db.Column(db.String)
-	name = db.Column(db.String)
-	
-	def __init__(self, user_name, password, name):
-		self.username = user_name
-		self.password = password
-		self.name = name
-	
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    
+    def __init__(self, user_name, password, name):
+        self.username = user_name
+        self.password = password
+        self.name = name
+    
 class Bids(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
@@ -42,45 +42,85 @@ class Bids(db.Model):
         self.currentbid = currentbid
         self.seller = seller
         self.pic = pic
-'''
+
 
 @app.route('/')
 @app.route("/home/")
 def landing():
-    return render_template('home.html')
+    global user
+    return render_template('home.html', user = user)
 @app.route('/echo/', methods = ['POST', 'GET'])
 def echo():
-	print "hello"
-	
+    print "hello"
+    
 @app.route('/browse')
 def browse():
+    global user
     bidsData = Bids.query.all()
-    return render_template('browse.html', bids = bidsData)
+    return render_template('browse.html', bids = bidsData, user = user)
 
 @app.route('/register')
 def register():
-	'''if request.method == 'POST':
-		user = request.form['username']
-		password = request.form['password']
-		name = request.form['name']
-		print name'''
-		
-	return render_template('register.html')
+    '''if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+        print name'''
+        
+    return render_template('register.html')
 
 @app.route('/signin', methods = ['POST', 'GET'])
 def signin():
-	
-	if request.method == 'POST':
-		user = request.form['username']
-		password = request.form['password']
-		name = request.form['name']
-		
-		new_user = Users(user, password,name)
-		db.session.add(new_user)
-		db.session.commit()
-		return'data recieved'
-	else:
-		return render_template('signin.html')
+    
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+        
+        new_user = Users(user, password,name)
+        db.session.add(new_user)
+        db.session.commit()
+        return'data recieved'
+    else:
+        return render_template('signin.html')
+
+@app.route('/verifyuser', methods = ['POST', 'GET'])
+def verifyuser():
+    global user
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print username
+        print password
+
+        #registered_user = session.query(Users).filter(Users.username = username).filter(Users.password = password)
+
+        #registered_user = session.query(Users).filter_by(username = username, password = password)
+        
+        registered_user = Users.query.filter_by(username = username, password = password).first()
+        #registered_user = Users.query.all()
+        print registered_user
+        if registered_user is None:
+            #flash('Username or Password is invalid' , 'error')
+            return 'Invalid username or password'
+            #return redirect(url_for('login'))
+        else:
+            user = username
+            #flash('Login successful')
+            print 'Login successful'
+            print user
+            return 'Login successful'
+
+@app.route('/profile')
+def profile():
+    global user
+    return render_template('profile.html', user = user)
+
+@app.route('/create')
+def create():
+    global user
+    return render_template('create.html', user = user)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
